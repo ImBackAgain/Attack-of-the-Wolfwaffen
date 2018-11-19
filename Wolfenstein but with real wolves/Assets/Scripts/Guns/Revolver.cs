ee9c50bb1f1369178b96d917715c401a33858878
Enan Munzar <kamenwati333@outlook.com>
2018-11-19 10:50:28 -0500

    Gun control

--- Wolfenstein but with real wolves/Assets/Scripts/Guns/Revolver.cs
+++ Wolfenstein but with real wolves/Assets/Scripts/Guns/Revolver.cs
@@ -3,5 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Revolver : Gun {
-    public Revolver() : base(12) { }
+    protected override void Initialize()
+    {
+        base.Initialize(12, 12, 0.2f, 1, 4);
+    }
 }
