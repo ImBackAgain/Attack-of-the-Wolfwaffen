984ba732a713517894e26777a3d88750b0d59393
Christian Brobst <chrisbrobst@hotmail.com>
2018-12-17 01:14:46 -0500

    end and win scene

--- Wolfenstein but with real wolves/Assets/Scripts/Player.cs
+++ Wolfenstein but with real wolves/Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : Lifeform {
     Gun revolver;
     AudioSource reload;
+    public GameObject evilBobRef;
 
     public Transform child;
 
@@ -70,6 +72,16 @@ public class Player : Lifeform {
             {
                 spells[spelllName].Cast();
             }
+        }
+
+        if(health <= 0)
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        if(evilBobRef == null)
+        {
+            SceneManager.LoadScene(2);
         }
 	}
 
